1f3bc760cc658e21b25fe4e1e2ee19d9774a1199
kosuke.ikeda <ikdksk>
2022-07-10 10:11:25 +0000

    first

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,21 @@
+def fizzbuzz(num)
+     if num % 3 == 0
+         puts "Fizz"
+     elsif num % 5 ==0
+         puts "Buzz"
+     elsif num % 3 == 0 && num % 5 == 0
+         puts "FizzBuzz"
+     else 
+         puts num.to_s
+     end  
+end
+
 num_max = 100
 (1..num_max).each do |num|
- def fizzbuzz(num)
-      if num % 3 == 0
-          puts "Fizz"
-      elsif num % 5 ==0
-          puts "Buzz"
-      elsif num % 3 == 0 && num % 5 == 0
-          puts "FizzBuzz"
-      else 
-          puts num.to_s
-      end  
- end
-    fizzbuzz(num)
+    puts fizzBuzz(num) 
 end
+
+puts fizzbuzz(1)
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
