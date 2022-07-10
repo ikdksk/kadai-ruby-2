1dbd76a386f75b28fe2d81d625393513cf931c4d
kosuke.ikeda <ikdksk>
2022-07-10 14:05:57 +0000

    修正2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,6 +1,4 @@
-num_max = 100
-(1..num_max).each do |num|
- def fizzbuzz(num)
+def fizzbuzz(num)
     if num % 3 == 0 && num % 5 == 0
         puts "FizzBuzz"
     elsif num % 3 == 0
@@ -10,6 +8,9 @@ num_max = 100
     else 
         puts num.to_s
     end  
- end
-    fizzbuzz(num)
+end
+
+num_max = 100
+(1..num_max).each do |num|
+  fizzbuzz(num)
 end
